f842e1ed36c7620d1e4381473611a5a177626761
gustavomorais <gustavomorais.dev@gmail.com>
2022-12-06 18:54:09 -0300

    Completa o requisito bônus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,38 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+const generatePhoneNumber = (array) => {
+
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  const count = {};
+
+  for (num of array) {
+    !count[num] ? count[num] = 1 : count[num] += 1;
+    if (num < 0 || num > 9 || count[num] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let phoneNumber = `(${array[0]}${array[1]}) `;
+
+  for (let index = 2; index <= 6; index += 1) {
+    phoneNumber += array[index];
+  }
+
+  phoneNumber += '-';
+
+  for (let index = 7; index <= 10; index += 1) {
+    phoneNumber += array[index];
+  }
+
+  return phoneNumber;
+}
+
+/* Nota : Aprendi uma boa prática ao contar elementos de uma array, que é criando um objeto e armazenando a quantidade de vezes que cada
+elemento aparece. */
+
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
@@ -8,7 +41,7 @@
 
 // Não modifique essas linhas
 module.exports = {
-  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => {}),
-  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => {}),
-  hydrate: typeof hydrate === 'function' ? hydrate : (() => {}),
+  generatePhoneNumber: typeof generatePhoneNumber === 'function' ? generatePhoneNumber : (() => { }),
+  triangleCheck: typeof triangleCheck === 'function' ? triangleCheck : (() => { }),
+  hydrate: typeof hydrate === 'function' ? hydrate : (() => { }),
 };
